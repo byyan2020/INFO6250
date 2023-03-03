cdb47705b7cf651fb31ed3afba200e74f6e54f51
Biyu Yan <yanbiyu2019@outlook.com>
2023-03-02 22:35:47 -0800

    Complete js-cart project

---
+++ work/07-js-cart/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+module.exports = {
+	mode: "development",
+	entry: "./src/cart.js",
+	devtool: "source-map",
+	output: {
+		filename: "cart.js",
+		path: path.resolve(__dirname, "public"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: { presets: ["@babel/preset-env"] },
+				},
+			},
+		],
+	},
+    devServer: {
+        static: path.join(__dirname, 'public'),
+        compress: true,
+        port: 5000
+      },
+      
+};
